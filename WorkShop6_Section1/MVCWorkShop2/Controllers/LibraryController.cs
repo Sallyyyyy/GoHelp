eeca6e253f62eb8bb376335cee25bfe33436f968
Lee szu-hsuan <sally20309@gmail.com>
2019-07-09 10:34:08 +0800

    workshop6_section1

---
+++ WorkShop6_Section1/MVCWorkShop2/Controllers/LibraryController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LB.Model;
+using LB.Service;
+
+namespace MVCWorkShop2.Controllers
+{
+    public class LibraryController : Controller
+    {
+        private ILBService lbService { get; set; }
+        // GET: Library
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult Index(LBSearchArg viewresult)
+        {
+            List<LBBooks> bookList = lbService.GetLibraryData(viewresult);
+            return this.Json(bookList);
+        }
+        //查詢書籍
+        [HttpPost]
+        public JsonResult Search(LBSearchArg viewresult)
+        {
+            List<LBBooks> bookList = lbService.SearchBook(viewresult);
+            return this.Json(bookList);
+        }
+        //類別下拉式選單
+        [HttpPost]
+        public JsonResult ClassDropDown()
+        {
+            List<LBBooks> bookClassList = lbService.BookClassDrop();
+            return this.Json(bookClassList);
+        }
+        //類別下拉式選單
+        [HttpPost]
+        public JsonResult StatusDropDown()
+        {
+            List<LBBooks> bookClassList = lbService.BookStatusDrop();
+            return this.Json(bookClassList);
+        }
+        [HttpPost]
+        public JsonResult KeeperDropDown()
+        {
+            List<LBBooks> bookClassList = lbService.BookKeeperDrop();
+            return this.Json(bookClassList);
+        }
+        [HttpPost]
+        public JsonResult Insert(LBSearchArg insertData)
+        {
+            return this.Json(lbService.InsertBook(insertData));
+        }
+
+
+    }
+}
