f9e5c963fbb3dd47882ab75e333255eb9945cc24
Lee szu-hsuan <sally20309@gmail.com>
2019-07-09 16:09:36 +0800

    Workshop6 section2 (單元測試)

---
+++ WorkShop6_Section2/MVCWorkShop2Tests/Controllers/LibraryControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVCWorkShop2.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using LB.Model;
+using LB.Service;
+
+
+namespace MVCWorkShop2.Controllers.Tests
+{
+    [TestClass]
+    public class LibraryControllerTests
+    {
+     
+
+        [TestMethod]
+        public void IndexTest()
+        {
+            var result = new LBSearchArg();
+            result.BookKeeper = "張三";
+            var controller = new LibraryController();
+            var testRes = controller.Search(result);
+            Assert.AreEqual(1, testRes);
+        }
+    }
+}
